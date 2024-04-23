9ec6f60cdfb156dbf95d335fd9d7d1d691947c1a
bmmcneil <zercher001@gmail.com>
2024-04-23 10:27:27 -0700

    Added generative function

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The project from Codecademy is freeform, so the generated message can be of any 
 
 I will be creating a barbarian warcry generator that will provide a free, random, unique warcry for you!
 There may or may not be some hidden *legendary* warcries--
+
+This may not be the most useful script, but I figure something like this may find some use in an RPG Battle Dialogue generator. Probably not the way it is. But that's the idea!
--- msgGenerator.js
+++ msgGenerator.js
@@ -2,16 +2,28 @@
 We will also need a way to randomize a number between 3 and 10, to determine how many syllables we should pull from this list. 
 After determining the number of iterations, we'll randomly pull that many syllables and return a unique warcry for the user. */
 
-//Array of unique syllabes; syllableArr.length = 32;
+//Array of unique syllabes;
 
-syllableArr = ['AA', 'GH', 'OO', 'NEH', 'II', 'EE', 'HOU', 'HUUB', 'JOCK', 'KAH', 'RO', 'ZII', 'FOS', 'RU', 'DAH', 'HAA','WIN', 'DOO','DUH', 'BEE', 'WUN', 'UU', 'OO', 'HUUN', 'GOON', 'SUH', 'MUH', 'MAH', 'MEH', 'RAG', 'NAR', 'OK', '*gulp*'];
+syllableArr = ['AA', 'AGH', 'OO', 'NEEH', 'II', 'EE', 'HOUU', 'HUUUB', 'JOOOK', 'KAAAH', 'ROO', ' *deep breath* ', 'FUUS', 'ROUH', 'DAAH', 'HAA','WIN', 'DOOO','DUUUH', 'BEE', 'WUN', 'UU', 'OO', 'HUUN', 'GOON', 'SUUUH', 'MUUH', 'MAAH', 'MEEH', 'RAG', 'NAR', 'OK', ' *gulp* '];
 
 /* Function below is to generator a random number within a desired range; a single syllable would not suffice--
 the function is flexible in case we'd like to change the range at any time */
 
 function randomNumber(min, max){
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 }
+
+let warCry = '';
+let counter = 0;
+function createWarCry(){
+    while (counter < randomNumber(2, 5)){
+        warCry += syllableArr[Math.floor(Math.random() * syllableArr.length)];
+        counter++
+    }
+    return warCry;
+}
+
+console.log(createWarCry());
 
 
 
